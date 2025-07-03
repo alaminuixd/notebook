b4bed6aa4b80c60a354e8260d867ea37a14336af
alamin khan <alaminuixd@gmail.com>
2025-07-04 02:56:44 +0600

    production build

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import router from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,13 +11,23 @@ dotenv.config();
 console.log(process.env.MONGO_URI);
 const app = express();
 const PORT = process.env.PORT || 3001;
+const __dirname = path.resolve();
+// console.log(__dirname);
 
 // middlewares
-app.use(customCors); // use this if you don't use "cors"
-// app.use(cors()); // use this if you don't use customCors
+if (process.env.NODE_ENV !== "production") {
+  app.use(customCors); // use this if you don't use "cors"
+}
+// app.use(cors("http://localhost:5174/")); // use this if you don't use customCors
 app.use(express.json()); // middleware to parse JSON body: / req.body
 app.use(rateLimiter);
 app.use("/api/notes", router); // routing middleware
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// in production we don't have "localhost"
+const BASE_URL =
+  import.meta.env.MODE === "production" ? "http://localhost:3001/api" : "/api";
+
 const axiosApi = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: BASE_URL,
 });
 
 export default axiosApi;
