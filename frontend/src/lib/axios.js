79ddfd1c63f73d4656aebaea18c7029ec8df2903
alamin khan <alaminuixd@gmail.com>
2025-07-06 16:39:54 +0600

    updated axios.js in the frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // in production we don't have "localhost"
 const BASE_URL =
-  import.meta.env.MODE === "production" ? "http://localhost:3001/api" : "/api";
+  import.meta.env.MODE === "production"
+    ? "https://notebook-6mda.onrender.com/api"
+    : "/api";
 
 const axiosApi = axios.create({
   baseURL: BASE_URL,
